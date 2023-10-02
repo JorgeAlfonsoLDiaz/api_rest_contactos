b3efb96734e968211fba82f63fa972c663453c69
Jorge Alfonso Luqueño Díaz <1722110483@utectulancingo.edu.mx>
2023-10-02 14:24:59 +0000

    Se ha añadido función para GET

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import csv
+import json
+
 from typing import Union
 
 from fastapi import FastAPI
@@ -7,13 +10,17 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"mesage": "Hello World"}
+    return {"message": "Hello World"}
 
 @app.get("/v1/contactos")
 async def get_contactos():
-    # TODO read contactos.csv
-    # TODO JSON encode contactos.csv
-    # TODO Guardar en response
+    file_csv = "contactos.csv"
     response = []
+
+    with open(file_csv, mode='r', newline='') as file:
+        reader_csv = csv.DictReader(file)
+        for row in reader_csv:
+            response.append(row)
+    response = json.dumps(response, indent=4)
     return response
 
