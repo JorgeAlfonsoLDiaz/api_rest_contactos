6382b8ec861f84825712e7ab30f11d7867e410a6
Jorge Alfonso Luqueño Díaz <1722110483@utectulancingo.edu.mx>
2023-10-04 15:33:08 +0000

    Se ha añadido la función POST de la API

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import json
 
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Contacto(BaseModel):
+    id_contactos: int
+    nombre: str
+    primer_apellido: str
+    segundo_apellido: str
+    email: str
+    telefono: str | None
 
 @app.get("/")
 def read_root():
@@ -21,6 +29,15 @@ async def get_contactos():
         reader_csv = csv.DictReader(file)
         for row in reader_csv:
             response.append(row)
-    response = json.dumps(response, indent=4)
     return response
+
+@app.post("/v1/contactos")
+async def post_contacto(contacto: Contacto):
+    file_csv = "contactos.csv"
+
+    with open(file_csv, mode="a", newline='') as file:
+        writer_csv = csv.DictWriter(file, fieldnames=["id_contactos", "nombre", "primer_apellido", "segundo_apellido", "email", "telefono"])
+        writer_csv.writerow({"id_contactos": contacto.id_contactos, "nombre": contacto.nombre, "primer_apellido": contacto.primer_apellido, "segundo_apellido": contacto.segundo_apellido, "email": contacto.email, "telefono": contacto.telefono})
+    
+    return {"message": "Contacto registrado exitosamente"}
 
